8694216d4c3cf484b5a5e928ddb57ac6f3fa6d52
Carl Düvel <c.a.duevel@gmail.com>
2019-04-03 11:01:59 +0200

    Add cider dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user 
-  {:plugins [
-     [lein-ns-dep-graph "0.2.0-SNAPSHOT"]
-     [lein-ancient "0.6.15"]
-     [lein-kibit "0.1.5"] 
-     [lein-try "0.4.3"]
-    ]
-  }
-  :signing {:gpg-key "2048R/B44D04C7"}
-}
+ {:plugins [
+            [lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+            [lein-ancient "0.6.15"]
+            [lein-kibit "0.1.6"] 
+            [lein-try "0.4.3"]]}
+ :signing {:gpg-key "2048R/B44D04C7"}
+ :repl {:plugins [[cider/cider-nrepl "0.21.1"
+                  refactor-nrepl "2.4.0"]]}}
+
