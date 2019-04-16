574c6f4c10584724c4f8e9f0e04b8367e1c661be
Carl Düvel <c.a.duevel@gmail.com>
2019-04-16 14:03:18 +0200

    Add yaml layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -58,6 +58,7 @@ values."
      chrome
 
      thesaurus
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
