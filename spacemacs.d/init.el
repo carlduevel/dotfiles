653e153192c91d5f6c307dacaafdce93ad647e9d
Carl Düvel <c.a.duevel@gmail.com>
2019-10-02 09:40:00 +0200

    Add html-to-hiccup package

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -63,7 +63,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(yasnippet-snippets all-the-icons)
+   dotspacemacs-additional-packages '(yasnippet-snippets all-the-icons html-to-hiccup)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
