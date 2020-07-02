0bf9a78ea6810453edd4b82e713b9197ea378c3b
Carl Düvel <c.a.duevel@gmail.com>
2020-07-02 11:39:15 +0200

    add hasp

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user 
- {:plugins [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]
-            [lein-ancient "0.6.15"]]}
- :signing {:gpg-key "2048R/B44D04C7"}}
+ {:plugins      [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+                 [lein-ancient "0.6.15"]]
+  :signing      {:gpg-key "2048R/B44D04C7"}
+  :dependencies [[hashp "0.2.0"]]
+  :injections   [(require 'hashp.core)]}}
 
